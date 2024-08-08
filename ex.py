a58a2e23a733c41f79ff53c7a521eab3528e80a2
mntiuser <mnti4694@gmail.com>
2024-08-08 10:27:41 +0530

    jkl

--- ex.py
+++ ex.py
@@ -1,1 +1,9 @@
-print("hello")
+import pandas as p 
+import numpy as n 
+d={"Day":[1,2,3,4,5,6,7,8,9,10],
+   "steps":[4335,9552,7332,4504,5335,7552,8332,6504,8965,7689]}
+dp=p.DataFrame(d)
+dp["+1000 steps"]=dp["steps"]+1000
+fi=dp[dp["+1000 steps"]>7000]["Day"]
+print("DataFrame:\n",dp)
+print("\n Days on which steps were>7000:\n",fi)
